7ad83ec0fa1560148e1599cf8234d57d2396ccb5
xyReaper2 <72480519+xyReaper2@users.noreply.github.com>
2026-03-24 01:07:58 -0300

    Update 01_Misc.lua

--- Elohim/01_Misc.lua
+++ Elohim/01_Misc.lua
@@ -1,1 +1,19 @@
+UI.Button("Hotkeys", function(newText)
+  UI.MultilineEditorWindow(storage.ingame_hotkeys or "", {title="Hotkeys editor", description="Adicione suas scripts aqui!"}, function(text)
+    storage.ingame_hotkeys = text
+    reload()
+  end)
+end)
 
+
+
+for _, scripts in pairs({storage.ingame_hotkeys}) do
+  if type(scripts) == "string" and scripts:len() > 3 then
+    local status, result = pcall(function()
+      assert(load(scripts, "ingame_editor"))()
+    end)
+    if not status then 
+      error("Hotkeys:\n" .. result)
+    end
+  end
+end
